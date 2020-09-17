505b17c7eaf1d96457821bb19e7b82132821580c
mvraa <40834090+mvraa@users.noreply.github.com>
2020-09-17 17:38:00 +0200

    gdfg

---
+++ Legacy.Test.Unit/LegacyUnitTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+
+//test
+namespace Legacy.Test.Unit
+{
+    [TestFixture]
+    public class LegacyUnitTest
+    {
+        private ECS uut;
+        [SetUp]
+        public void Setup()
+        {
+            uut = new ECS(20, new FakeTempSensor(), new FakeHeater());
+        }
+
+        [TestCase(10, 10)]
+        [TestCase(15, 15)]
+        [TestCase(20, 20)]
+
+        public void TestGetThreshHold(int threshold, int val)
+        {
+            val = uut.GetThreshold();
+            Assert.That(val, Is.EqualTo(val));
+        }
+
+        [Test]
+        public void TestSetTemp()
+        {
+
+        }
+    }
+}
